2b93a05ec10819f171f009b96d001a928a538651
Bojana Paunović <145152009+paunovicbojana@users.noreply.github.com>
2024-04-04 15:51:33 +0200

    modified:   main_page.html new file:   organizatori/belgrade_urban_fest.html new file:   organizatori/beoart_fest.html new file:   organizatori/dunavski_horizonti.html new file:   organizatori/etno-srbija.html new file:   organizatori/etnofest_beograd.html new file:   organizatori/gari_fest.html new file:   organizatori/lazarevac_fest.html new file:   organizatori/nis_fest.html renamed:    organizatori.txt -> organizatori/organizatori.txt renamed:    organizatori_festivala.json -> organizatori/organizatori_festivala.json new file:   organizatori/urban_beat.html new file:   read_more.js modified:   style.css

---
+++ read_more.js
@@ -0,0 +1,36 @@
+document.getElementById("rm1").addEventListener("click", function() {
+    window.location.href = "organizatori/beoart_fest.html";
+});
+
+document.getElementById("rm2").addEventListener("click", function() {
+    window.location.href = "organizatori/etno_srbija.html";
+});
+
+document.getElementById("rm3").addEventListener("click", function() {
+    window.location.href = "organizatori/lazarevac_fest.html";
+});
+
+document.getElementById("rm4").addEventListener("click", function() {
+    window.location.href = "organizatori/dunavski_horizonti.html";
+});
+
+document.getElementById("rm5").addEventListener("click", function() {
+    window.location.href = "organizatori/urban_beat.html";
+});
+
+document.getElementById("rm6").addEventListener("click", function() {
+    window.location.href = "organizatori/gari_fest.html";
+});
+
+document.getElementById("rm7").addEventListener("click", function() {
+    window.location.href = "organizatori/etnofest_beograd.html";
+});
+
+document.getElementById("rm8").addEventListener("click", function() {
+    window.location.href = "organizatori/belgrade_urban_fest.html";
+});
+
+document.getElementById("rm9").addEventListener("click", function() {
+    window.location.href = "organizatori/nis_fest.html";
+});
+
