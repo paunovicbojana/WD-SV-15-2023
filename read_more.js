9c18016e5469f35a3f47ede4cbe45fe9d813cd43
Bojana Paunović <145152009+paunovicbojana@users.noreply.github.com>
2024-04-05 18:29:31 +0200

    modified:   main_page.html renamed:    organizatori/belgrade_urban_fest.html -> organizers/belgrade_urban_fest.html renamed:    organizatori/etno-srbija.html -> organizers/beoart_fest.html renamed:    organizatori/dunavski_horizonti.html -> organizers/dunavski_horizonti.html renamed:    organizatori/beoart_fest.html -> organizers/etno_srbija.html renamed:    organizatori/etnofest_beograd.html -> organizers/etnofest_beograd.html renamed:    organizatori/gari_fest.html -> organizers/gari_fest.html renamed:    organizatori/lazarevac_fest.html -> organizers/lazarevac_fest.html renamed:    organizatori/nis_fest.html -> organizers/nis_fest.html renamed:    organizatori/organizatori.txt -> organizers/organizatori.txt renamed:    organizatori/organizatori_festivala.json -> organizers/organizatori_festivala.json renamed:    organizatori/urban_beat.html -> organizers/urban_beat.html modified:   read_more.js modified:   style.css

--- read_more.js
+++ read_more.js
@@ -1,36 +1,36 @@
 document.getElementById("rm1").addEventListener("click", function() {
-    window.location.href = "organizatori/beoart_fest.html";
+    window.location.href = "organizers/beoart_fest.html";
 });
 
 document.getElementById("rm2").addEventListener("click", function() {
-    window.location.href = "organizatori/etno_srbija.html";
+    window.location.href = "organizers/etno_srbija.html";
 });
 
 document.getElementById("rm3").addEventListener("click", function() {
-    window.location.href = "organizatori/lazarevac_fest.html";
+    window.location.href = "organizers/lazarevac_fest.html";
 });
 
 document.getElementById("rm4").addEventListener("click", function() {
-    window.location.href = "organizatori/dunavski_horizonti.html";
+    window.location.href = "organizers/dunavski_horizonti.html";
 });
 
 document.getElementById("rm5").addEventListener("click", function() {
-    window.location.href = "organizatori/urban_beat.html";
+    window.location.href = "organizers/urban_beat.html";
 });
 
 document.getElementById("rm6").addEventListener("click", function() {
-    window.location.href = "organizatori/gari_fest.html";
+    window.location.href = "organizers/gari_fest.html";
 });
 
 document.getElementById("rm7").addEventListener("click", function() {
-    window.location.href = "organizatori/etnofest_beograd.html";
+    window.location.href = "organizers/etnofest_beograd.html";
 });
 
 document.getElementById("rm8").addEventListener("click", function() {
-    window.location.href = "organizatori/belgrade_urban_fest.html";
+    window.location.href = "organizers/belgrade_urban_fest.html";
 });
 
 document.getElementById("rm9").addEventListener("click", function() {
-    window.location.href = "organizatori/nis_fest.html";
+    window.location.href = "organizers/nis_fest.html";
 });
 
